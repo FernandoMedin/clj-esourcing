eada6653e173e66312e09dddfc71900ec8ff3172
Fernando Pereira Medin <fernando-medin@hotmail.com>
2017-08-01 01:51:04 -0300

    Add function

--- src/clj_esourcing/core.clj
+++ src/clj_esourcing/core.clj
@@ -3,11 +3,35 @@
   (:require [monger.core :as mg]
             [monger.collection :as mc]))
 
+;; Mongo config
 (def conn (mg/connect))
 (def db   (mg/get-db conn "logs"))
-(def disc (mg/disconnect))
+
+;; Cols
+(def log-coll "logs")
+
+(defn parse-int [x]
+  (Integer. (re-find #"[0-9]*" x)))
+
+(defn get-input []
+  (println "Choose an option: ")
+    (read-line))
+
+
+(defn add-log [payload]
+  "Payload -> keyword"
+  (mc/insert db "logs" {:msg (get payload :msg)
+                        :type (get payload :type)
+                        :created_at (new java.util.Date)})
+  (println "ADD"))
+
+(defn intro [val]
+  (cond
+    (= (parse-int val) 1) (add-log {:msg "Add a log" :type "ADD_LOG"})
+    :else (println "Bye!")))
 
 (defn -main
   "docstring."
   [& args]
-  (println "Init clj-esoucing-demo"))
+  (println "Init clojure event soucing demo")
+  (intro (get-input)))
